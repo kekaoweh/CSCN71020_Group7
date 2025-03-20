729239e9bb0ad58cd6a6a4fa4da557d2438e16c0
kekaaoweh <koweh9232@conestogac.on.ca>
2025-03-20 15:25:05 -0400

    Created a native unit test project and also a .h file with all the functions stated there. Linked the .h file to the unittests file.

---
+++ CSCN71020_P1-main/source.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int validTriangle(double a, double b, double c);
+void calculateTriangle_Angles(double a, double b, double c);
+void get_triangle_input();
+double distance(double x1, double y1, double x2, double y2);
+int is_rectangle(double p1, double p2, double p3, double p4, double diag1, double diag2);
+int is_straight_line(double x[], double y[]);
+int has_duplicate_points(double x[], double y[]);
+void sortRectangle_point(double x[], double y[]);
+void get_rectangle_input();
---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+#include "../CSCN71020_P1-main/source.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
