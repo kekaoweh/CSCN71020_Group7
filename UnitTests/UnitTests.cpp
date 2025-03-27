dfcd1ad5566283048205f77edcd366d96540a49f
kekaaoweh <koweh9232@conestogac.on.ca>
2025-03-26 22:43:45 -0400

    wrote the unit test cases for valid triangle and fixed some errors in the main source file. Handled some debugging errors in the unittest project.

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -7,14 +7,35 @@ extern "C" {
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTests
+namespace TriangleRectangleTests
 {
-	TEST_CLASS(UnitTests)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
+    TEST_CLASS(TriangleTests)
+    {
+    public:
+
+        // Test for a valid triangle
+        TEST_METHOD(ValidTriangleTest)
+        {
+            Assert::IsTrue(validTriangle(3, 4, 5));
+            Assert::IsTrue(validTriangle(5, 12, 13));
+            Assert::IsTrue(validTriangle(7, 24, 25));
+        }
+
+        // Test for invalid triangles where sum of two sides is less than or equal to the third side
+        TEST_METHOD(InvalidTriangleTest)
+        {
+            Assert::IsFalse(validTriangle(1, 2, 5));
+            Assert::IsFalse(validTriangle(1, 2, 3));
+            Assert::IsFalse(validTriangle(2, 2, 5));
+        }
+
+        // Test where the sides are the same, but not a valid triangle
+        TEST_METHOD(EquilateralTest)
+        {
+            Assert::IsTrue(validTriangle(5, 5, 5));
+            Assert::IsFalse(validTriangle(0, 0, 0));  // all sides are 0
+        }
+
+    };
+
 }
